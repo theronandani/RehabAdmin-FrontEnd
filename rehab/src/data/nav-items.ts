ff0901d1937b1797f2caffa6a58f6f8c922c378e
Andani Baloyi <theronandani@gmail.com>
2025-07-02 22:45:53 +0200

    Rehab Admin

---
+++ rehab/src/data/nav-items.ts
@@ -0,0 +1,62 @@
+export interface NavItem {
+  title: string;
+  path: string;
+  icon?: string;
+  active: boolean;
+  collapsible: boolean;
+  sublist?: NavItem[];
+}
+
+const navItems: NavItem[] = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'ion:home-sharp',
+    active: true,
+    collapsible: false,
+    
+  },
+
+  {
+    title: 'Dashboard',
+    path: '#!',
+    icon: 'icomoon-free:drawer',
+    active: true,
+    collapsible: false,
+
+  },
+    {
+    title: 'View Applications',
+    path: '#!',
+    icon: 'mingcute:grid-fill',
+    active: true,
+    collapsible: false,
+
+  },
+
+  {
+    title: 'User Management',
+    path: 'authentication',
+    icon: 'tabler:shopping-bag',
+    active: true,
+    collapsible: false,
+  },
+  
+   
+  {
+    title: 'Notification',
+    path: '#!',
+    icon: 'zondicons:notifications',
+    active: false,
+    collapsible: false,
+  },
+  {
+    title: 'Message',
+    path: '#!',
+    icon: 'ph:chat-circle-dots-fill',
+    active: false,
+    collapsible: false,
+  },
+];
+
+export default navItems;
